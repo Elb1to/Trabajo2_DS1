ed0365df54a8b1525eb54f70c6f48739822d2a0d
Elb1to <elbitomc@gmail.com>
2020-05-20 15:48:21 -0500

    Added main gui options (just text, no selector yet)

--- main.c
+++ main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <io.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+void main() {
+    /*
+     * Here we'll declarate the variables for the main method
+     * integers: option;
+     */
+    int option, loopOptions;
+    printf("\n\n\n[]===========================================[]\n");
+    printf("Selecciona un algoritmo que desees probar.\n\n");
+    printf("1. Costo de lapices\n");
+    printf("2. Almacen Factory\n");
+    printf("3. Restaurante\n");
+    printf("4. Vinicultores\n");
+    printf("5. Viaje de estudios\n");
+    printf("6. Envio de paquetes internacional\n");
+    printf("7. Estacionamientos\n");
+    printf("8. Edad de 3 personas\n");
+    printf("9. Presidente de la republica\n");
+    printf("10. Sueldo semanal\n");
+    printf("[]===========================================[]\n");
+    getch();
 }
